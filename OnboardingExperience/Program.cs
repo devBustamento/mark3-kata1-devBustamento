994e6a85ec175428caba794ca9672af1d610547c
devBustamento <dev.bustamento@gmail.com>
2018-04-11 12:27:44 -0500

    got User class talking to main method

--- OnboardingExperience/Program.cs
+++ OnboardingExperience/Program.cs
@@ -6,12 +6,21 @@ namespace OnboardingExperience
     {
         static void Main(string[] args)
         {
+
+            var U = new User();
+
             Console.WriteLine("Welcome to my Onbaording app, we need some info to get started...");
             Console.WriteLine("\nPress any key to continue...");
             Console.ReadKey(true);
-        
+
+            Console.WriteLine("\nWhat is your First name? ");
+            U.FirstName = Console.ReadLine();
+            Console.WriteLine($"\nHello, {U.LastName}, what is your last name?");
+
             Console.Write("\nPress any key to exit...");
-            Console.ReadKey(true); //commit
+            //Console.ReadKey(true); //commit
         }
+
+
     }
 }
