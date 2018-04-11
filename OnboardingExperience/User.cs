f615b69c7efd1c92da1357bd0415110f12c1d9df
devBustamento <dev.bustamento@gmail.com>
2018-04-11 12:28:29 -0500

    got User class talking to main method

---
+++ OnboardingExperience/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnboardingExperience
+{
+    class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int AccNo { get; set; }
+        public bool AccOwn { get; set; }
+    }
+}
