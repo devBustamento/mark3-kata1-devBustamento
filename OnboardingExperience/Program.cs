545e0e0e23debb380b5f1553cf7b01627f3091da
devBustamento <dev.bustamento@gmail.com>
2018-04-11 13:37:07 -0500

    refactored into a method rather than being so redundant

--- OnboardingExperience/Program.cs
+++ OnboardingExperience/Program.cs
@@ -7,20 +7,33 @@ namespace OnboardingExperience
         static void Main(string[] args)
         {
 
-            var U = new User();
+            var user = new User();
 
             Console.WriteLine("Welcome to my Onbaording app, we need some info to get started...");
             Console.WriteLine("\nPress any key to continue...");
             Console.ReadKey(true);
 
-            Console.WriteLine("\nWhat is your First name? ");
-            U.FirstName = Console.ReadLine();
-            Console.WriteLine($"\nHello, {U.LastName}, what is your last name?");
+            user.FirstName = AskQuestion("\nWhat is your First name? ");
+            Console.WriteLine($"\nHello, {user.FirstName},");
+
+            user.LastName = AskQuestion("\nwell...what is your last name?");
+            Console.WriteLine($"\nOkay {user.FirstName} {user.LastName}, lets keep it rollin'");
+
 
             Console.Write("\nPress any key to exit...");
             Console.ReadKey(true); //commit
         }
 
+        static string AskQuestion(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
+        }
 
+        static int NumQuestion(string question)
+        {
+            Console.WriteLine(question);
+            return Convert.ToInt32(Console.ReadLine());
+        }
     }
 }
