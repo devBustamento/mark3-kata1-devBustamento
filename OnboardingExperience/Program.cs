6aefbf7d49d03f1ed659780ff4c6cf393b068a25
devBustamento <dev.bustamento@gmail.com>
2018-04-11 10:31:44 -0500

    my first commit, application opens, greets, and closes with user input

--- OnboardingExperience/Program.cs
+++ OnboardingExperience/Program.cs
@@ -6,9 +6,13 @@ namespace OnboardingExperience
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to my Onbaording app, we need some info to get started...");
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey(true);
+        
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true); //commit
 
-            Console.ReadLine();
         }
     }
 }
